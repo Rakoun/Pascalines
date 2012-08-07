8d458b8bd143e7f77ab0558918c94953523cb548
Régis GEROMEGNACE <regis.geromegnace@gmail.com>
2012-08-06 23:37:41 -0400

    Implémentation MVVM
    Création des classe BaseModel et ConverterModel

---
+++ Pascalines/Model/BaseModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pascalines.Model
+{
+    public partial class BaseModel : INotifyPropertyChanged
+    {
+        public BaseModel()
+        {
+        }
+
+        //TODO: ajouter membres ou méthodes utilisables par les classes filles
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Déclenche l'évènement PropertyChanged pour une propriété
+        /// donnée.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="exp">L'expression permettant de retrouvler la
+        /// propriété.</param>
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> exp)
+        {
+            var memberExpression = exp.Body as MemberExpression;
+            if (memberExpression != null)
+            {
+                string propertyName = memberExpression.Member.Name;
+                PropertyChangedEventHandler handler = PropertyChanged;
+                if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion // INotifyPropertyChanged Members
+    }
+}
---
+++ Pascalines/Model/Tools/ConverterModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pascalines.Model.Tools
+{
+    public partial class ConverterModel : BaseModel
+    {
+        private string _calculationLine;
+        public string CalculationLine
+        {
+            get
+            {
+                return _calculationLine;
+            }
+            set
+            {
+                if (_calculationLine == value) return;
+                _calculationLine = value;
+                RaisePropertyChanged<string>(() => CalculationLine);
+            }
+        }
+
+        private decimal _calculationLineResult;
+        public decimal CalculationLineResult
+        {
+            get
+            {
+                return _calculationLineResult;
+            }
+            set
+            {
+                if (_calculationLineResult == value) return;
+                _calculationLineResult = value;
+                RaisePropertyChanged<decimal>(() => CalculationLineResult);
+            }
+        }
+
+        private decimal _percentage;
+        public decimal Percentage
+        {
+            get
+            {
+                return _percentage;
+            }
+            set
+            {
+                if (_percentage == value) return;
+                _percentage = value;
+                RaisePropertyChanged<decimal>(() => Percentage);
+            }
+        }
+        private decimal _percentageValue;
+        public decimal PercentageValue
+        {
+            get
+            {
+                return _percentageValue;
+            }
+            set
+            {
+                if (_percentageValue == value) return;
+                _percentageValue = value;
+                RaisePropertyChanged<decimal>(() => PercentageValue);
+            }
+        }
+        private decimal _referenceAmount;
+        public decimal ReferenceAmount
+        {
+            get
+            {
+                return _referenceAmount;
+            }
+            set
+            {
+                if (_referenceAmount == value) return;
+                _referenceAmount = value;
+                RaisePropertyChanged<decimal>(() => ReferenceAmount);
+            }
+        }
+
+        private decimal _percentVAT; //% TVA
+        public decimal PercentVAT
+        {
+            get
+            {
+                return _percentVAT;
+            }
+            set
+            {
+                if (_percentVAT == value) return;
+                _percentVAT = value;
+                RaisePropertyChanged<decimal>(() => PercentVAT);
+            }
+        }
+        private decimal _valueVAT; // valeur TVA
+        public decimal ValueVAT
+        {
+            get
+            {
+                return _valueVAT;
+            }
+            set
+            {
+                if (_valueVAT == value) return;
+                _valueVAT = value;
+                RaisePropertyChanged<decimal>(() => ValueVAT);
+            }
+        }
+
+        private decimal _allTaxesExcluded; // HT
+        private decimal _inclusiveOfTaxes; // TTC
+
+        private decimal _markupRage; // TM (taux de marque)
+        private decimal _k;
+
+        private decimal _crudeProfit; // Marge brute
+        private decimal _sellingPriceAllTaxeExcluded; // prix de vente HT
+
+    }
+}
+
