b21b09f03bdfd9599a09a4b628b1cbbe827867bf
Régis GEROMEGNACE <regis.geromegnace@gmail.com>
2012-08-09 23:42:48 -0400

    Implémentation de MVVM
    poursuite de la réorganisation du code et adaptation de l'architecture à
    Windows 8

---
+++ Pascalines.Framework/Messages/DialogService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace Pascalines.Framework
+{
+    public class DialogService : IDialogService
+    {
+        // inspiré de : http://tinyurl.com/9kyg48f
+        async public Task<bool> AskConfirmation(string title, string message)
+        {
+            var messageDialog = new MessageDialog(message, title);
+
+            // ajout des commandes et positionnement de leur id
+            messageDialog.Commands.Add(new UICommand("OK",null, 0));
+            messageDialog.Commands.Add(new UICommand("Cancel", null, 1));
+
+            // définition de la commande par défaut
+            messageDialog.DefaultCommandIndex = 1;
+
+            // affiche la boite de dialogue et récupère l'évènement invoqué
+            // par l'opérateur async
+            IUICommand result = await messageDialog.ShowAsync();
+
+            return (int)result.Id == 0;
+            
+        }
+
+        async public void DisplayInformation(string title, string message)
+        {
+            var messageDialog = new MessageDialog(message, title);
+            await messageDialog.ShowAsync();
+        }
+    }
+}
---
+++ Pascalines.Framework/Messages/IDialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pascalines.Framework
+{
+    public interface IDialogService
+    {
+        Task<bool>  AskConfirmation(string title, string message);
+        void DisplayInformation(string title, string message);
+    }
+}
